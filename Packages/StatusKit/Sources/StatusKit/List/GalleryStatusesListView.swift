d4a9db8f389f3114bd96de90d21baba41ed152e8
jadetheda <54419064+jadetheda@users.noreply.github.com>
2026-07-29 11:33:39 +0000

    Fix exit code 65 by removing extra loadedAspectRatio argument in GalleryAspectRatioModifier

--- Packages/StatusKit/Sources/StatusKit/List/GalleryStatusesListView.swift
+++ Packages/StatusKit/Sources/StatusKit/List/GalleryStatusesListView.swift
@@ -322,7 +322,6 @@ public struct GalleryMediaCell: View {
   @State private var showSelectableText: Bool = false
   @State private var isBlockConfirmationPresented = false
   @State private var isShareAsImageSheetPresented = false
-  @State private var loadedAspectRatio: CGFloat? = nil
 
   public var body: some View {
     let isSquare = UserPreferences.shared.galleryCropToSquare
@@ -363,7 +362,7 @@ public struct GalleryMediaCell: View {
             EmptyView()
           }
         }
-        .modifier(GalleryAspectRatioModifier(isSquare: isSquare, aspectRatio: mediaStatus.attachment.aspectRatio, loadedAspectRatio: loadedAspectRatio))
+        .modifier(GalleryAspectRatioModifier(isSquare: isSquare, aspectRatio: mediaStatus.attachment.aspectRatio))
         .clipped()
         .clipShape(RoundedRectangle(cornerRadius: UserPreferences.shared.galleryRoundCorners ? 8 : 0))
         .contentShape(RoundedRectangle(cornerRadius: UserPreferences.shared.galleryRoundCorners ? 8 : 0))
