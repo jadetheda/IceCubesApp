47ed8a0cf44e593e16e472af622f161773407813
anna <54419064+jadetheda@users.noreply.github.com>
2026-07-30 15:40:17 -0400

    Fix Bluesky theme colors to be exact matches

--- Packages/DesignSystem/Sources/DesignSystem/ColorSet.swift
+++ Packages/DesignSystem/Sources/DesignSystem/ColorSet.swift
@@ -210,10 +210,12 @@ public struct ThreadsLight: ColorSet {
 public struct BlueskyDark: ColorSet {
   public var name: ColorSetName = .blueskyDark
   public var scheme: ColorScheme = .dark
-  public var tintColor: Color = .init(hex: 0x6394F4)
-  public var primaryBackgroundColor: Color = .init(hex: 0x161D26)
-  public var secondaryBackgroundColor: Color = .init(hex: 0x161D26)
+  public var tintColor: Color = .init(hex: 0x4D97FF)
+  public var primaryBackgroundColor: Color = .init(hex: 0x151D28)
+  public var secondaryBackgroundColor: Color = .init(hex: 0x151D28)
   public var labelColor: Color = .white
+}
+
 
   public init() {}
 }
@@ -221,7 +223,7 @@ public struct BlueskyDark: ColorSet {
 public struct BlueskyLight: ColorSet {
   public var name: ColorSetName = .blueskyLight
   public var scheme: ColorScheme = .light
-  public var tintColor: Color = .init(hex: 0x2B69F8)
+  public var tintColor: Color = .init(hex: 0x006AFF)
   public var primaryBackgroundColor: Color = .init(hex: 0xFFFFFF)
   public var secondaryBackgroundColor: Color = .init(hex: 0xFFFFFF)
   public var labelColor: Color = .black
