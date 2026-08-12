f768393f0b750304fe00fbd81d1580f246607967
AIStudio <aistudio@google.com>
2026-08-12 10:59:32 +0000

    Clean up temporary debugging files

--- test_ext.swift
+++
@@ -1,4 +0,0 @@
-import Foundation
-let urls = ["https://denden.world/system/media_attachments/files/117/073/332/642/322/887/original/05e447891271dcc4.mp4"]
-let allExts = urls.compactMap { URL(string: $0)?.pathExtension.lowercased() }
-print(allExts)
